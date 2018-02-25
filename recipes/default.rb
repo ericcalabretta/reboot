b96dc4f254880ac826006574bd22d7290c8b7c34
ericcalabretta <eric.calabretta@gmail.com>
2018-02-25 16:36:04 -0600

    first

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+reboot 'restart_computer' do
+  action :nothing
+  reason "testing test-kitchen"
+  delay_mins 1
+end
+
+file 'c:\test' do
+  content'test reboot by changing this & converge'
+  action :create
+  notifies :reboot_now, 'reboot[restart_computer]', :immediately
+end
